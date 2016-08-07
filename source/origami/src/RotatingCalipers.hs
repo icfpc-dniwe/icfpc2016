40808c7b7249e01e6756091f5e0425c3e36bcfff
alvearian <am-vi@yandex.ru>
2016-08-07 18:44:19 +0300

    calipers

--- source/origami/src/RotatingCalipers.hs
+++ source/origami/src/RotatingCalipers.hs
@@ -5,10 +5,17 @@ import qualified Data.Set as S
 
 type ConvexHull = S.Set VR
 
-max' :: (VR, Double) -> (VR, Double) -> (VR, Double)
-max' t0 t1 = if snd t0 > snd t1 then t0 else t1
+v2DotProduct :: VR -> VR -> Rational
+v2DotProduct (V2 xa ya) (V2 xb yb) = (ya - xa) ^^ 2 + (yb - xb) ^^ 2
+
+v2Length :: VR -> Double
+v2Length v = sqrt $ v2DotProduct v v
+
+v2GetAngle :: VR -> VR -> Double
+v2GetAngle v0 v1 = acos $ v2DotProduct v0 v1 / (v2Length v0 * v2Length v1)
 
 getBoundingBox :: ConvexHull -> (Rational, Rational)
 getBoundingBox ch = 
 	let pts = S.toList ch,
 		lines = zip pts $ drop 1 $ cycle pts,
+		
